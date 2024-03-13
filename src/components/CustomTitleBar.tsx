7539741563e0c603ccc791299d6868eadbee794d
Thaddeus <thaddeuskrugman@gmail.com>
2024-03-13 16:15:24 -0400

    added minimize and maximize buttons

--- src/components/CustomTitleBar.tsx
+++ src/components/CustomTitleBar.tsx
@@ -4,23 +4,34 @@ import { appWindow } from '@tauri-apps/api/window'
 const CustomTitleBar: Component = () => {
   const handleClose = async () => {
     await appWindow.close()
-    alert('Window closed')
+  }
+
+  const handleMinimize = async () => {
+    await appWindow.minimize()
+  }
+
+  const handleMaximize = async () => {
+    await appWindow.maximize()
   }
 
   return (
     <div
       data-tauri-drag-region='true'
       id='title-bar'
-      class='flex flex-row justify-between px-6 lg:px-8 py-5 cursor-default items-start'
+      class='flex flex-row space-x-2 px-6 lg:px-8 py-5 cursor-default items-start'
     >
-      <div class='rounded-full flex h-3 w-3 items-center justify-center bg-gruv-gray-dark hover:bg-close-red group cursor-default ease-in-out transition-colors duration-150'>
-        <button
-          onClick={() => handleClose()}
-          class='text-xs text-gruv-gray-dark group-hover:text-black/50 cursor-default'
-        >
-          x
-        </button>
-      </div>
+      <button
+        onClick={() => handleClose()}
+        class='rounded-full flex h-3 w-3 items-center justify-center bg-gruv-gray-dark hover:bg-close-red group cursor-default ease-in-out transition-colors duration-150'
+      />
+      <button
+        onClick={() => handleMinimize()}
+        class='rounded-full flex h-3 w-3 items-center justify-center bg-gruv-gray-dark hover:bg-gruv-yellow group cursor-default ease-in-out transition-colors duration-150'
+      />
+      <button
+        onClick={() => handleMaximize()}
+        class='rounded-full flex h-3 w-3 items-center justify-center bg-gruv-gray-dark hover:bg-gruv-aqua group cursor-default ease-in-out transition-colors duration-150'
+      />
     </div>
   )
 }
--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -18,10 +18,10 @@ const ListItem: Component<ListItemProps> = (props) => {
       <div class='h-[3px] opacity-0 group-hover:opacity-100 bg-gruv-gray-dark/20 flex-grow mx-8'></div>
       <div class='flex opacity-0 group-hover:opacity-100 overflow-hidden items-center'>
         <button class='mr-4' classList={{ hidden: props.completed }}>
-          <CheckIcon size={18} color='hover:text-gruv-aqua' />
+          <CheckIcon size={20} color='hover:text-gruv-aqua' />
         </button>
         <button>
-          <CloseIcon size={18} color='hover:text-gruv-purple' />
+          <CloseIcon size={20} color='hover:text-close-red' />
         </button>
       </div>
     </div>
